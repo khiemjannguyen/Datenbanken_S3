b29c1a3f9b456d088fe73fda8bbe7436cec7d420
khiemjannguyen <khiemjan.nguyen@gmail.com>
2020-10-10 21:49:52 +0200

    Aufgabe: Datenbank added

--- Hausaufgabe_2/Hausaufgabe_2.sql
+++ Hausaufgabe_2/Hausaufgabe_2.sql
@@ -1,36 +1,36 @@
 CREATE TABLE Projekt(
-	ProjektID int NOT NULL,
-    Projektbezeichnung mediumtext,
+		ProjektID int NOT NULL,
+    Projektbezeichnung varchar(255),
+    Projektkurzbeschreibung mediumtext,
     Startdatum date,
     Abschlussdatum date,
-    ProjektleiterID int,
-    MitarbeiterID int,
+    ProjektleiterID int NOT NULL,
+    MitarbeiterID int NOT NULL,
     PRIMARY KEY (ProjektID),
     FOREIGN KEY (ProjektleiterID) REFERENCES Projektleiter (ProjektleiterID),
     FOREIGN KEY (MitarbeiterID) REFERENCES Mitarbeiter (MitarbeiterID)
 );
 
-CREATE TABLE MITARBEITER(
-	MitarbeiterID int NOT NULL,
-    ProjektleiterID int NOT NULL,
+CREATE TABLE Mitarbeiter(
+		MitarbeiterID int NOT NULL,
+    ProjektleiterID boolean NOT NULL,
     Vorname varchar(25) NOT NULL,
     Nachname varchar(25) NOT NULL,
-    Resort varchar(50) NOT NULL,
+    ResortID varchar(50) NOT NULL,
     PRIMARY KEY (MitarbeiterID),
+    FOREIGN KEY (ResortID) REFERENCES Resort (ResortID),
     FOREIGN KEY (ProjektleiterID) REFERENCES Projektleiter (ProjektleiterID)
 );
 
 CREATE TABLE Projektleiter(
-	ProjektleiterID int NOT NULL,
+		ProjektleiterID int NOT NULL,
     ProjektID int NOT NULL,
     PRIMARY KEY (ProjekleiterID),
     FOREIGN KEY (ProjektID) REFERENCES Projekt (ProjektID)
 );
 
 CREATE TABLE Resort(
-	ResortID int NOT NULL,
+		ResortID int NOT NULL,
     Bezeichnung mediumtext,
-    MitarbeiterID int,
-    PRIMARY KEY (ResortID),
-    FOREIGN KEY (MitarbeiterID) REFERENCES MITARBEITER (MitarbeiterID)
+    PRIMARY KEY (ResortID)
 );
